8ca7acd64fffa422613fedeb376b299f97ddcf27
V1torMiranda <vitor.academico14@gmail.com>
2025-08-05 15:48:31 -0300

    chore: salvar alterações locais antes do pull

--- src/utils/ConsoleUtils.cpp
+++ src/utils/ConsoleUtils.cpp
@@ -1,3 +1,20 @@
 //
 // Created by vitor on 01/08/2025.
 //
+
+#include "ConsoleUtils.h"
+#include <iostream>
+
+void ConsoleUtils::printTituloCentralizado(const std::string& titulo, int largura) {
+    if ((int)titulo.size() >= largura) {
+        std::cout << titulo << "\n";
+        return;
+    }
+
+    int espacos = (largura - (int)titulo.size()) / 2;
+    std::string linha(largura, '=');
+
+    std::cout << linha << "\n";
+    std::cout << std::string(espacos, ' ') << titulo << "\n";
+    std::cout << linha << "\n";
+}
--- src/utils/ConsoleUtils.h
+++ src/utils/ConsoleUtils.h
@@ -6,3 +6,15 @@
 #define BIBLIOTECA_COMUNITARIA_CONSOLEUTILS_H
 
 #endif //BIBLIOTECA_COMUNITARIA_CONSOLEUTILS_H
+
+#ifndef CONSOLE_UTILS_H
+#define CONSOLE_UTILS_H
+
+#include <string>
+
+class ConsoleUtils {
+public:
+    static void printTituloCentralizado(const std::string& titulo, int largura = 80);
+};
+
+#endif
